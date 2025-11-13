3072cabf9288ab2dac20771ebb68e2d9c7afeb5d
mtobdvlb <mtobdvlb@163.com>
2025-11-14 00:45:26 +0800

    feat(web): 实现登录页面及组件拆分
    - 新增登录页面路由组件，包含背景样式和布局结构- 创建 LoginWrapper 组件封装密码和邮箱验证码登录表单- 拆分登录模型组件为独立文件，优化代码组织结构
    - 更新 .gitignore 文件排除所有 .env.* 文件
    - 调整登录模型相关组件的样式命名和属性传递逻辑
    -优化输入框焦点处理逻辑，使用可选链避免潜在错误
    - 更新IDEA项目配置文件，排除新增的开发目录和文件

---
+++ apps/web/src/app/login/page.tsx
@@ -0,0 +1,20 @@
+import LoginWrapper from '@/features/login/components/LoginWrapper'
+
+const Login = () => {
+  return (
+    <div className={'mx-auto min-w-[1100px] max-w-[2560px] min-h-[calc(100vh-540px)] bg-white'}>
+      <div className={'w-full h-[106px] bg-[linear-gradient(#2ca0d8,#2ca0d8_86px,#fff_0,#fff)]'}>
+        <div
+          className={"w-[980px] h-[106px] mx-auto bg-[url('/images/login-top.png')] bg-no-repeat"}
+        ></div>
+      </div>
+      <div className={'mx-auto pt-2.5 '}>
+        <div className={'w-[405px] mx-auto pt-[82px] pb-[108px]'}>
+          <LoginWrapper />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ apps/web/src/features/login/components/LoginWrapper.tsx
@@ -0,0 +1,85 @@
+'use client'
+
+import { Separator, Tabs, TabsContent, TabsList, TabsTrigger } from '@/components'
+import { cn } from '@/lib'
+import LoginModelPassword from '@/components/layout/models/login-model/LoginModelPassword'
+import LoginModelCode from '@/components/layout/models/login-model/LoginModelCode'
+import { useState } from 'react'
+import { tv } from 'tailwind-variants'
+
+const loginFormStyles = tv({
+  slots: {
+    item: 'relative flex justify-start items-center w-full h-11 py-3 px-5',
+    label: 'flex-nowrap whitespace-nowrap block shrink-0 m-0 p-0 font-normal',
+    input: 'flex-1 min-w-0 ml-5 border-none shadow-[inset_0_0_0_1000px_#fff] focus-visible:ring-0 ',
+    btnWrap: 'flex justify-between mt-5 w-100 font-normal leading-10 text-center',
+    btnOther:
+      'w-[194px] shadow-none h-10 cursor-pointer hover:bg-white font-normal text-[#18191c] bg-white border border-[#e3e5e7] rounded-[8px] m-0 p-0',
+    btnPrimary:
+      'bg-[#00aeec] shadow-none w-[194px] h-10 cursor-pointer font-normal hover:bg-[#00aeec] hover:opacity-85 text-white rounded-[8px] m-0 p-0',
+  },
+})
+
+const LoginWrapper = () => {
+  const [isPasswordOpen, setIsPasswordOpen] = useState(false)
+
+  const formStyles = loginFormStyles()
+
+  const [tabsValue, setTabsValue] = useState<'password' | 'code'>('password')
+  return (
+    <Tabs className={'flex flex-col justify-center items-center w-full'} value={tabsValue}>
+      <TabsList
+        className={'m-0 mb-6 flex w-full justify-center border-none bg-transparent p-0'}
+        defaultValue={'password'}
+      >
+        <TabsTrigger
+          onClick={() => setTabsValue('password')}
+          className={cn(
+            'focus-visible:shadow-none focus-visible:ring-0 data-[state=active]:shadow-none',
+            'm-0 cursor-pointer p-0 text-lg leading-5 font-medium text-[#505050] data-[state=active]:cursor-not-allowed data-[state=active]:text-[#4fa5d9]'
+          )}
+          value={'password'}
+        >
+          密码登录
+        </TabsTrigger>
+        <Separator
+          className={'mx-[21px] my-0 h-5 rounded-[8px] bg-[#e3e5e7] p-0'}
+          orientation={'vertical'}
+        />
+        <TabsTrigger
+          onClick={() => setTabsValue('code')}
+          className={cn(
+            'focus-visible:shadow-none focus-visible:ring-0 data-[state=active]:shadow-none',
+            'm-0 cursor-pointer p-0 text-lg leading-5 font-medium text-[#505050] data-[state=active]:cursor-not-allowed data-[state=active]:text-[#4fa5d9]'
+          )}
+          value={'code'}
+        >
+          邮箱登录
+        </TabsTrigger>
+      </TabsList>
+      <TabsContent
+        className={
+          'text-text1 relative m-0 rounded-none p-0 text-sm leading-[1.6] font-normal select-none'
+        }
+        value={'password'}
+      >
+        <LoginModelPassword
+          isPasswordOpen={isPasswordOpen}
+          setIsPasswordOpen={setIsPasswordOpen}
+          formStyles={formStyles}
+          setTabsValue={setTabsValue}
+        />
+      </TabsContent>
+      <TabsContent
+        className={
+          'text-text1 relative m-0 rounded-none p-0 text-sm leading-[1.6] font-normal select-none'
+        }
+        value={'code'}
+      >
+        <LoginModelCode formStyles={formStyles} />
+      </TabsContent>
+    </Tabs>
+  )
+}
+
+export default LoginWrapper
